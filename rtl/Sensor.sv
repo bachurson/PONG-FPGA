b50c1350cb89abde76410afdf41f5c090b360fe1
WiktorDziedzic <133821072+WiktorDziedzic@users.noreply.github.com>
2023-07-21 19:35:44 +0200

    Update Sensor.sv
    Poprawione zgodnie z specyfikacja

--- rtl/Sensor.sv
+++ rtl/Sensor.sv
@@ -18,33 +18,36 @@
 // Additional Comments: Pierwszy test zegarow do wysylania poprawnego sygnalu na Trigger czujnika HC-SR04 
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
-
 module Sensor(
     input logic clk,
     input logic btnC,
     input logic Echo,
     output logic Trig,
     output logic [8:0] led
-
 );
 
     logic [32:0] counter;
     logic [32:0] counter_nxt;
      
 always_ff @(posedge clk) begin 
-       if(counter < 20_000_000) begin
+    if(btnC) begin
+        counter <= 0;
+        counter_nxt <= 0;
+        led <= 9'b000000000; 
+    end
+    else if(counter < 20_000_000) begin
             led <= 9'b111111111;
-            
-            counter <= counter + 1;
+
+            counter_nxt <= counter + 1;
+            counter <= counter_nxt;
        end
        else if (counter >= 20_000_000 && counter < 100_000_000) begin
             led <= 9'b000000000;
-            counter <= counter + 1;
+            counter_nxt <= counter + 1;
+            counter <= counter_nxt;
        end
        else 
             counter <= 0;
-       end
-       
-endmodule
+end
+
+endmodule 
