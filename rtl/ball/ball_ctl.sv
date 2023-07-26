0940a55c4da83df92f6d1cd353e2f12e8e6b50f5
Dominik Bachurski <dominikbachurski02@gmail.com>
2023-07-27 00:13:57 +0200

    creating PLAY state

--- rtl/ball/ball_ctl.sv
+++ rtl/ball/ball_ctl.sv
@@ -3,10 +3,15 @@
 `timescale 1 ns / 1 ps
 
 module ball_ctl (
-    input  logic clk,
-    input  logic rst,
-    input  logic [10:0] rect_y_pos,
-
+    input logic clk,
+    input logic rst,
+    input logic [10:0] rect_y_pos,
+    //input logic [1:3] random_3,
+    //input logic [1:4] random_4,
+    //input logic [1:5] random_5,
+    input logic [2:0] random_3,
+    input logic [3:0] random_4,
+    input logic [4:0] random_5,
 
     output logic [10:0] xpos,
     output logic [10:0] ypos
@@ -22,6 +27,16 @@ module ball_ctl (
  logic [19:0] divider_nxt;
  logic signed [10:0] dir;
  logic signed [10:0] dir_nxt;
+ logic [4:0] angle_counter_x;
+ logic [4:0] angle_x_nxt;
+ logic [4:0] angle_counter_y;
+ logic [4:0] angle_y_nxt;
+ logic [4:0] random;
+ logic [4:0] random_nxt;
+ logic IN_RECT;
+ logic LOW_PRECISION;
+ logic MEDIUM_PRECISION;
+ logic HIGH_PRECISION;
  import vga_pkg::*;
 
 
@@ -30,15 +45,20 @@ module ball_ctl (
     localparam START = 2'b00;
     localparam PLAY = 2'b01;
     localparam SCORE = 2'b10;
-    localparam [31:0] COUNTER_MAX = 50_000_000;
+    localparam [26:0] COUNTER_MAX = 50_000_000;
     localparam RECT_X_POSITION = 30;
     localparam RECT_HEIGHT = 100;
     localparam RECT_WIDTH = 15;
+    //localparam IN_RECT = (xpos == RECT_X_POSITION + RECT_WIDTH + 1 && ypos >= rect_y_pos && ypos <= rect_y_pos + RECT_HEIGHT);
+    //localparam LOW_PRECISION = (xpos == RECT_X_POSITION + RECT_WIDTH && ((ypos >= rect_y_pos && ypos <= rect_y_pos + 15) || (ypos >= rect_y_pos + 85 && ypos <= rect_y_pos + RECT_HEIGHT )));
+    //localparam MEDIUM_PRECISION = (xpos == RECT_X_POSITION + RECT_WIDTH && ((ypos >= rect_y_pos + 15 && ypos <= rect_y_pos + RECT_HEIGHT - 65) || (ypos >= rect_y_pos + 65 && ypos <= rect_y_pos + RECT_HEIGHT - 15)));
+    //localparam HIGH_PRECISION = (xpos == RECT_X_POSITION + RECT_WIDTH && ypos >= rect_y_pos + 35 && ypos <= rect_y_pos + RECT_HEIGHT - 35);
+
     
 
 //procedural
 //********************************************************************//
-    always @(posedge clk) begin
+    always_ff @(posedge clk) begin
       if (rst) begin
         dir <= -1;
         state <= START;
@@ -46,6 +66,9 @@ module ball_ctl (
         counter <= '0;
         xpos <= X_CENTER;
         clock_divider <= 0;
+        angle_counter_x <= 0;
+        angle_counter_y <= 0;
+        random <= 0;
       end
       else begin
         xpos <= xpos_nxt;
@@ -55,17 +78,30 @@ module ball_ctl (
         counter <= counter_nxt;
         clock_divider <= divider_nxt;
         dir <= dir_nxt;
+        angle_counter_x <= angle_x_nxt; 
+        angle_counter_y <= angle_y_nxt; 
+        random <= random_nxt;
       end 
     end
 
 
   //combinational 
   //********************************************************************//
+  always_comb begin
+    IN_RECT = (xpos == RECT_X_POSITION + RECT_WIDTH  && ypos >= rect_y_pos && ypos <= rect_y_pos + RECT_HEIGHT);
+    LOW_PRECISION = (xpos == RECT_X_POSITION + RECT_WIDTH && ((ypos >= rect_y_pos && ypos <= rect_y_pos + 15) || (ypos >= rect_y_pos + 85 && ypos <= rect_y_pos + RECT_HEIGHT )));
+    MEDIUM_PRECISION = (xpos == RECT_X_POSITION + RECT_WIDTH && ((ypos >= rect_y_pos + 15 && ypos <= rect_y_pos + RECT_HEIGHT - 65) || (ypos >= rect_y_pos + 65 && ypos <= rect_y_pos + RECT_HEIGHT - 15)));
+    HIGH_PRECISION = (xpos == RECT_X_POSITION + RECT_WIDTH && ypos >= rect_y_pos + 35 && ypos <= rect_y_pos + RECT_HEIGHT - 35);
+  end
+
   always_comb begin 
     if (counter >= COUNTER_MAX) 
       counter_nxt = 0; 
     else 
-      counter_nxt = counter + 1 + velocity/10; 
+      if(state==PLAY)
+        counter_nxt = counter + 1 + velocity/3;
+      else 
+        counter_nxt = counter + 1 + velocity/10; 
 
     if(clock_divider == 1000000) begin //velocity setup
       divider_nxt = 0;
@@ -85,18 +121,27 @@ module ball_ctl (
     case(state)
         START: begin
           if (xpos > 0 && counter >= COUNTER_MAX) begin
-            if (xpos == RECT_X_POSITION + RECT_WIDTH && ypos >= rect_y_pos && ypos <= rect_y_pos + RECT_HEIGHT) begin
+            if (IN_RECT) begin
+              random_nxt = 0;
+              angle_x_nxt = 0;
+              angle_y_nxt = 0;
               dir_nxt = -dir;
-              xpos_nxt = xpos + dir;
+              xpos_nxt = xpos;
               ypos_nxt = Y_CENTER;
               state_nxt = PLAY;
             end else begin
+              random_nxt = 0;
+              angle_x_nxt = 0;
+              angle_y_nxt = 0;
               dir_nxt = dir;
               xpos_nxt = xpos + dir;
               ypos_nxt = Y_CENTER;
               state_nxt = START;
             end
           end else begin
+            random_nxt = 0;
+            angle_x_nxt = 0;
+            angle_y_nxt = 0;
             dir_nxt = dir;
             xpos_nxt = xpos;
             ypos_nxt = Y_CENTER;
@@ -105,22 +150,74 @@ module ball_ctl (
         end
  
         PLAY: begin
-          if (counter >= COUNTER_MAX) begin
-            dir_nxt = dir;
-            xpos_nxt = xpos + dir;
-            ypos_nxt = ypos;
-            state_nxt = PLAY;
+          if (xpos > 0 && xpos < HOR_PIXELS) begin
+            if (counter >= COUNTER_MAX) begin
+              if(IN_RECT) begin
+                angle_x_nxt = 0;
+                angle_y_nxt = 0;
+                dir_nxt = dir;
+                xpos_nxt = xpos + dir;
+                ypos_nxt = ypos + dir;
+                state_nxt = PLAY;
+                if(HIGH_PRECISION)
+                  random_nxt = random_3;
+                else if (MEDIUM_PRECISION)
+                  random_nxt = random_4;
+                else 
+                  random_nxt = random_5;
+              end else begin
+                random_nxt = random;
+                state_nxt = PLAY;
+                dir_nxt = dir;
+                if(angle_counter_x == 3) begin
+                  xpos_nxt = xpos + dir;
+                  angle_x_nxt = 0;
+                end else begin
+                  xpos_nxt = xpos;
+                  angle_x_nxt = angle_counter_x + 1; 
+                end
+                if(angle_counter_y == random) begin
+                  ypos_nxt = ypos + dir;
+                  angle_y_nxt = 0;
+                end else begin
+                  ypos_nxt = ypos;  
+                  angle_y_nxt = angle_counter_y + 1; 
+                end
+              end 
+            end else begin
+              random_nxt = random;
+              angle_x_nxt = angle_counter_x;
+              angle_y_nxt = angle_counter_y;
+              state_nxt = PLAY;
+              dir_nxt = dir;
+              xpos_nxt = xpos;
+              ypos_nxt = ypos;
+            end
           end else begin
+            random_nxt = 0;
+            angle_x_nxt = 0;
+            angle_y_nxt = 0;
+            state_nxt = SCORE;
             dir_nxt = dir;
             xpos_nxt = xpos;
-            state_nxt = PLAY;
             ypos_nxt = ypos;
           end
         end
 
-        //SCORE:
-        
+        SCORE: begin
+          random_nxt = 0;
+          angle_x_nxt = 0;
+          angle_y_nxt = 0;
+          dir_nxt = dir;
+          xpos_nxt = xpos;
+          ypos_nxt = ypos;
+          state_nxt = SCORE;    
+        end
+
         default: begin
+          random_nxt = 0;
+          angle_x_nxt = 0;
+          angle_y_nxt = 0;
           xpos_nxt = xpos;
           ypos_nxt = ypos;
           state_nxt = START;
--- rtl/top_vga.sv
+++ rtl/top_vga.sv
@@ -20,6 +20,9 @@ logic btn_down;
 logic [10:0] rect_y_pos;
 logic [10:0] ball_xpos;
 logic [10:0] ball_ypos;
+logic [1:3] random_3;
+logic [1:4] random_4;
+logic [1:5] random_5;
 
 // VGA signals from timing
 vga_if vga_tim();
@@ -95,11 +98,35 @@ draw_rect u_draw_rect (
     .vga_out(vga_rct)
 );
 
+random #(.N(3)) u_random_3 (
+    .clk,
+    .rst,
+
+    .Q(random_3)
+);
+
+random #(.N(4)) u_random_4 (
+    .clk,
+    .rst,
+    
+    .Q(random_4)
+);
+
+random #(.N(5)) u_random_5 (
+    .clk,
+    .rst,
+    
+    .Q(random_5)
+);
+
 ball_ctl u_ball_ctl (
     .clk,
     .rst,
     .rect_y_pos,
-
+    .random_3,
+    .random_4,
+    .random_5,
+    
     .xpos(ball_xpos),
     .ypos(ball_ypos)
 
