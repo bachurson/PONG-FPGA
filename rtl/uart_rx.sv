4fd998a260b9c1cf3f8f631c1023f127be8f8e97
WiktorDziedzic <133821072+WiktorDziedzic@users.noreply.github.com>
2023-08-20 17:44:16 +0200

    Update uart_rx.sv
    Improved readability

--- rtl/uart_rx.sv
+++ rtl/uart_rx.sv
@@ -20,95 +20,6 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-/*
-module uart_rx(
-    input logic clk,
-    input logic rst,
-    input logic RxD,
-    output logic [7:0] RxData
-);
-    
-    
- 
-logic shift;
-logic [3:0] bit_counter;
-logic [1:0] sample_counter;
-logic [13:0] baudrate_counter;
-logic [9:0] rxshift_reg;
-logic clear_bitcounter;
-logic inc_bitcounter;
-logic inc_samplecounter;
-logic clear_samplecounter;
-logic state, state_nxt;
-
-
-localparam clk_freq = 65_000_000;
-localparam baud_rate = 9_600;
-localparam div_sample = 4;
-localparam div_counter = clk_freq/(baud_rate*div_sample);
-localparam mid_sample = (div_sample/2);
-localparam div_bit = 10;
-
-assign RxData = rxshift_reg [8:1];
-
-always_ff@(posedge clk)
-    begin
-        if(rst) begin
-            state <= 0;
-            bit_counter <= 0;
-            baudrate_counter <= 0;
-            sample_counter <= 0;
-        end else begin
-            baudrate_counter <= baudrate_counter + 1;
-            if(baudrate_counter >= div_counter - 1) begin
-                baudrate_counter <= 0;
-                state <= state_nxt;
-                if(shift) rxshift_reg <= {RxD, rxshift_reg[9:1]};
-                if(clear_samplecounter) sample_counter <= 0;
-                if(inc_samplecounter) sample_counter <= sample_counter +1;
-                if(clear_bitcounter) bit_counter <= 0;
-                if(inc_bitcounter) bit_counter <= bit_counter +1;
-            end
-       end
-end
-
-always_ff@ (posedge clk) 
-begin
-    shift <= 0;
-    clear_samplecounter <= 0;
-    inc_samplecounter <= 0;
-    clear_bitcounter <= 0;
-    inc_bitcounter <= 0;
-    state_nxt <= 0;
-    
-    case (state)
-        0: begin
-            if(RxD)
-                begin
-                state_nxt <= 0;
-                end
-            else begin
-                state_nxt <= 1;
-                clear_bitcounter <= 1;
-                clear_samplecounter <= 1;
-            end
-        end
-        1: begin
-            state_nxt <= 1;
-            if(sample_counter == mid_sample - 1) shift <= 1;
-                if(sample_counter == div_sample - 1) begin
-                    if(bit_counter == div_bit - 1) begin
-                state_nxt <= 0;
-                end
-                inc_bitcounter <= 1;
-                clear_samplecounter <= 1;
-           end else inc_samplecounter <= 1;
-        end
-        default: state_nxt <= 0;
-    endcase
-end
-endmodule
-*/
 module uart_rx(
     input logic clk,
     input logic rst,
@@ -192,10 +103,12 @@ module uart_rx(
             inc_bitcounter_nxt = inc_bitcounter;
         end
 
-        shift_nxt = 0;
-        if (sample_counter == mid_sample - 1) shift_nxt = 1;
-
-
+        if (sample_counter == mid_sample - 1) 
+            shift_nxt = 1;
+        else 
+            shift_nxt = 0;
+            
+            
         clear_samplecounter_nxt = 0;
         inc_samplecounter_nxt = 0;
         clear_bitcounter_nxt = 0;
